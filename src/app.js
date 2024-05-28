a69326b6c430944410d6879d3e38b1614e093d8c
Parkseojin2 <bym401543@gmail.com>
2024-05-28 23:38:06 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import UsersRouter from './routers/users.router.js'
-
+import UsersRouter from './routers/users.router.js';
+import ResumeRouter from './routers/resumes.router.js';
+import errorHandler from './middlewares/error-handler.middleware.js';
 
 const app = express();
 const PORT = 3018;
 
+
 app.use(express.json());
 app.use(cookieParser());
 app.use('/auth', [UsersRouter]);
+app.use('/resumes', [ResumeRouter]);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(PORT, '포트로 서버가 열렸어요!');
